38cc2515e8d787d6f365e6518adb822ebb677c82
Jamey Evans <jevans@cdivalve.com>
2018-06-09 14:54:50 -0500

    Initial Commit
    Initial Commit

---
+++ data.js
@@ -0,0 +1,28 @@
+var nozzleLibrary = [
+	{
+		type: "nozzle",
+		partNumber: 401,
+		refrigerant: "410a",
+		minCircuit: {
+			fiveThirtySec: 2,
+			threeSixteen: 2,
+			quarter: 2,
+			fiveSixteen: 0,
+			threeEight: 0},
+		maxCircuit: {
+			fiveThirtySec: 7,
+			threeSixteen: 2,
+			quarter: 2,
+			fiveSixteen: 0,
+			threeEight: 0},
+		nozzleSize: "P",
+		hasSidePort: false
+	},
+	{
+		partNumber: "503",
+		tubeCount: 8,
+		hasSidePort: true
+	}
+];
+
+alert("data.js is active");
---
+++ kfapp.js
@@ -0,0 +1,8 @@
+var dataObject;
+dataObject = nozzleLibrary;
+
+for (var i = 0; i < dataObject.length; i++) {
+ console.log(dataObject[i]);
+};
+
+alert("kfapp.js is loaded");
